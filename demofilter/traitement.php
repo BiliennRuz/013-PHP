551469635ec73ca43143e674f908d89c78a61bf1
Adel LATIBI <alatibi@campus-eni.fr>
2022-05-18 11:54:21 +0200

    Filter champs

--- demofilter/traitement.php
+++ demofilter/traitement.php
@@ -2,9 +2,16 @@
     // verif email
     function checkEmail(&$email){
         $email = filter_var($_POST["email"],FILTER_SANITIZE_EMAIL);
-        if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+        // if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+        //     throw new Exception("Email non valide");
+        // }
+        if(!filter_var($email,FILTER_VALIDATE_REGEXP,array(
+            "options"=>array(
+                "regexp"=>"/^[a-zA-Z0-9\.\_\-]+\@[a-zA-Z0-9\.\_\-]+\.[a-zA-Z]{2,5}$/"
+            )
+        ))){
             throw new Exception("Email non valide");
-        }
+        }        
     }
 
     if(isset($_POST["submit"]) && isset($_POST["email"]) ){
